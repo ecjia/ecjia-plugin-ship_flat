8f3079a5160605d9fe316300be07b7b05823322b
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-28 11:52:11 +0800

    添加是否支持打印快递单模板配置项

--- config.php
+++ config.php
@@ -49,6 +49,7 @@ return array(
     'shipping_code' => 'ship_flat',
 	'insure' 		=> false,		/* 不支持保价 */
 	'cod' 			=> true, 		/* 配送方式是否支持货到付款 */
+    'print_support' => false,
 	'print_model'	=> 2,			/* 模式编辑器 */
 	'print_bg'		=> '',			/* 打印单背景 */
 	'config_lable'	=> '',			/* 打印快递单标签位置信息 */
